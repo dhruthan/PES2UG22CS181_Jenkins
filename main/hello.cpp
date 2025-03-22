1cef0d20608336ddbab635c917b781c196c4468a
dhruthan <117083709+dhruthan@users.noreply.github.com>
2025-03-22 23:17:01 +0530

    Updated the hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(void) {
+  cout << "Hey, I made change to the file, is auto trigger working ? oh yeah\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Configured by PES2UG22CS181\n";
