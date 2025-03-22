b4d4943fd89674dd5e631ea096042c8265d729d1
dhruthan <117083709+dhruthan@users.noreply.github.com>
2025-03-22 23:46:50 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Its Jenkins Pipeline\n";
+  cout << "Created by PES2UG22CS181\n";
+  return 0;
+}
