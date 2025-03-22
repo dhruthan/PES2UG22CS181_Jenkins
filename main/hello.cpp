eae9e5fad3b990c5e3817215274f877fac9c3946
dhruthan <117083709+dhruthan@users.noreply.github.com>
2025-03-22 22:51:24 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "Configured by PES2UG22CS181\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
